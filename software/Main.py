e5829c96a932cb29e78eb4b0e65257310d698464
Ryan DiCenzo <dicenzoryan@gmail.com>
2019-08-20 17:08:51 -0700

    Requesting help for bug

--- software/Main.py
+++ software/Main.py
@@ -8,8 +8,8 @@ def startWorld():
     maze = tm.get_tiles()
 
     astar = AStar(maze)
-    start = (0,0)
-    end = (99,84)
+    start = (10,0)
+    end = (10,10)
 
     path = astar.gen_path(start, end)
     astar.draw_path('floor_plan.png', path)
@@ -18,8 +18,10 @@ def startWorld():
     run_directions_from_path(path)
 
 def run_directions_from_path(path):
-    current_point = (0, 0)
-    margin = 0.1
+    current_point = path[0]
+    current_heading = -math.pi / 2
+
+    margin = 0.2
 
     straight_dist_traveled = 0
 
@@ -29,12 +31,14 @@ def run_directions_from_path(path):
         s2 = point[1] - current_point[1]
 
         dist = math.sqrt(s1 ** 2 + s2 ** 2)
+
         if (s1 == 0):
             angle = 0
         else:
-            angle = math.tan(s2 / s1)
+            angle = math.atan(s2 / s1)
 
-        if (angle < margin):
+
+        if (abs(angle) < margin):
             straight_dist_traveled += dist
             continue
 
@@ -42,10 +46,16 @@ def run_directions_from_path(path):
             # robot.move('straight', straight_dist_traveled, 0.5)
             straight_dist_traveled = 0
 
+
         # robot.move('turn', angle, 0.5)
-        print ('turned ' , angle , ' radians')
+        print ('turned ' , angle * (180/math.pi) , ' degrees')
         # robot.move('straight', dist, 0.5)
         print ('moved ' , dist , ' units')
 
         current_point = point
+
+    if (straight_dist_traveled != 0):
+            # robot.move('straight', straight_dist_traveled, 0.5)
+        print ('move ', straight_dist_traveled)
+
 startWorld()
